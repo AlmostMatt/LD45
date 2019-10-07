ea05fcebbc84f9d21d3897ececcad442fba65db0
Matthew Hyndman <myndman@gmail.com>
2019-10-07 08:50:40 -0700

    Add an AudioPlayer component and script. Also add audiolistener to the camera

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AudioClipIndex { IMPACT=0, PAPER = 1, PICKUP = 2};
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    private static AudioPlayer instance;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    // call anywhere with code like AudioPlayer.PlaySound(AudioClipIndex.IMPACT);
+    public static void PlaySound(AudioClipIndex index)
+    {
+        Debug.Log("Playing sound!");
+        instance.GetComponent<AudioSource>().PlayOneShot(instance.audioClips[(int)index]);
+    }
+}
