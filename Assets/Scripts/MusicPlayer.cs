e83796ca6386e8cc0a7e644e26560d42f2cb882b
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2019-10-07 16:51:52 -0400

    background music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public bool MusicEnabled = true;
+
+    void Start()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    void Update()
+    {
+    }
+
+    public void ToggleMusic()
+    {
+        MusicEnabled = !MusicEnabled;
+        if (MusicEnabled)
+            GetComponent<AudioSource>().Pause();
+        else
+            GetComponent<AudioSource>().UnPause();
+    }
+}
